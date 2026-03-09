c0a3b04a68bf3b853c9ada5f6dfef58d69a2ad01
Lucky Virgiawan Dwi Widodo <luckyvirgiawan24@gmail.com>
2026-03-09 21:28:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',   
+        secondary: '#16A34A', 
+        background: '#F8FAFC',
+      },
+    },
+  },
+  plugins: [],
+}
